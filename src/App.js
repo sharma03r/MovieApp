86e5018234063a9b9d5c82e3ed6951073bfe66af
Rahul Sharma <sharmarahul8848@gmail.com>
2023-12-20 20:48:50 +0530

    Added the movie card and designed the movie-card

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import MovieCard from "./Moviecard";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Movie App</h1>
+      <MovieCard />
     </>
   );
 }
--- src/Moviecard.js
+++ src/Moviecard.js
@@ -2,7 +2,29 @@ import { Component } from "react";
 
 class MovieCard extends Component {
   render() {
-    return <h2>Movie Card</h2>;
+    return (
+      <div className="main">
+        <div className="movie-card">
+          <div className="left">
+            <img
+              alt="poster"
+              src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+            />
+          </div>
+          <div className="right">
+            <div className="title">The avengers</div>
+            <div className="plot">lorem</div>
+            <div className="price">Rs 199</div>
+            <div className="footer">
+              <div className="rating">8.9</div>
+              <div className="stars">Stars</div>
+              <div className="favourite-btn">Favourite</div>
+              <div className="cart-btn">Add to Cart</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
