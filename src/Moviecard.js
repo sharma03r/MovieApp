8a3eb6d4321f0e4cef53dc34b423e8e6ce5df8a3
Rahul Sharma <sharmarahul8848@gmail.com>
2023-12-23 13:17:16 +0530

    Adding stars via + button

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,7 +1,25 @@
 import { Component } from "react";
 
 class MovieCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "The Avengers",
+      plot: "Supernatural heroes",
+      price: 199,
+      rating: 8.9,
+      stars: 0,
+    };
+  }
+  addStars = () => {
+    this.setState((prevState) => {
+      return {
+        stars: prevState.stars + 0.5,
+      };
+    });
+  };
   render() {
+    const { title, plot, price, rating, stars } = this.state;
     return (
       <div className="main">
         <div className="movie-card">
@@ -12,12 +30,30 @@ class MovieCard extends Component {
             />
           </div>
           <div className="right">
-            <div className="title">The avengers</div>
-            <div className="plot">lorem</div>
-            <div className="price">Rs 199</div>
+            <div className="title">{title}</div>
+            <div className="plot">{plot}</div>
+            <div className="price">Rs {price}</div>
             <div className="footer">
-              <div className="rating">8.9</div>
-              <div className="stars">Stars</div>
+              <div className="rating">{rating}</div>
+              <div className="star-dis">
+                <img
+                  className="str-btn"
+                  alt="decrease"
+                  src="https://cdn-icons-png.flaticon.com/128/9146/9146915.png"
+                />
+                <img
+                  className="stars"
+                  alt="star"
+                  src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png"
+                />
+                <img
+                  className="str-btn"
+                  alt="increase"
+                  src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png"
+                  onClick={this.addStars}
+                />
+                <span> {stars}</span>
+              </div>
               <div className="favourite-btn">Favourite</div>
               <div className="cart-btn">Add to Cart</div>
             </div>
